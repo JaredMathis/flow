b0f4ebe8dbf5aeb7654a9e63e5a6529565acb035
JaredMathis <jaredmathis@protonmail.com>
2020-06-06 07:43:44 -0400

    update

--- library/execute.js
+++ library/execute.js
@@ -9,7 +9,7 @@ function execute(name, inputs, outputs) {
     u.scope(execute.name, x => {
         u.merge(x,{name, inputs, outputs})
         u.assert(() => args.length === 3);
-        u.assert(() => u.isString(name));
+        u.assert(() => name === null || u.isString(name));
         u.assert(() => u.isDefined(inputs));
         u.assert(() => u.isDefined(outputs));
 
