c4ea6de6b21866c067e3387bf5d627e6f95f98e9
JaredMathis <jaredmathis@protonmail.com>
2020-06-08 12:39:08 -0400

    update

--- tests/defineCount/defineCount.js
+++ tests/defineCount/defineCount.js
@@ -4,5 +4,15 @@ const u = require("wlj-utilities");
 const defineCount = require("../../library/defineCount.js");
 
 u.scope(__filename, x => {
-    // TODO
+    let log = false;
+
+    let count = defineCount();
+    
+    let expected = require('./expected');
+    try {
+        u.assertIsEqualJson(() => count, () => expected);
+    } catch (e) {
+        console.log(JSON.stringify(count, null, 2));
+        throw e;
+    }
 });
